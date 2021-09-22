35e5cfd16589ac8f49c5723483f41752a749d61e
Owner <samih1079@gmail.com>
2021-09-22 13:23:34 +0300

    ...

---
+++ app/src/main/java/neveen/samih/samihtasksmanager/SignInActivity.java
@@ -0,0 +1,14 @@
+package neveen.samih.samihtasksmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
