46ec3af25532874f87bebd0892b181f05f1e6fa2
Owner <samih1079@gmail.com>
2021-11-10 08:32:58 +0200

    spalsh acrean by thread

--- app/src/main/java/neveen/samih/samihtasksmanager/SpalshScreenActivity.java
+++ app/src/main/java/neveen/samih/samihtasksmanager/SpalshScreenActivity.java
@@ -2,6 +2,7 @@ package neveen.samih.samihtasksmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SpalshScreenActivity extends AppCompatActivity {
@@ -10,5 +11,25 @@ public class SpalshScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spalsh_screen);
+        //Thread: 1
+        Thread th=new Thread(){
+            //Thread: 2
+            @Override
+            public void run() {
+                //هنا المقطع الذي سيعمل بالتزامن مع مقاطع اخرى
+                //Thread:3
+                int ms=3*1000;//milliseconds
+                try {
+                    sleep(ms);
+                    startActivity(new Intent(getApplicationContext(),MainTasksActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        //Thread: 4
+        th.start();
+
+
     }
 }
